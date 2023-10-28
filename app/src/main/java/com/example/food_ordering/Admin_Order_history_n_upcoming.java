fa0815bfee1803cdf4c8cf45dd461b470c9f86a3
Ng Wei Jie <p2101355@student.newinti.edu.my>
2023-10-28 19:46:56 +0800

    order

--- app/src/main/java/com/example/food_ordering/Admin_Order_history_n_upcoming.java
+++ app/src/main/java/com/example/food_ordering/Admin_Order_history_n_upcoming.java
@@ -1,37 +1,34 @@
 package com.example.food_ordering;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
-import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
 import java.util.ArrayList;
 import java.util.List;
+import android.util.Log;
 
-public class Admin_Order_history_n_upcoming extends AppCompatActivity {
 
-
+public class Admin_Order_history_n_upcoming extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MyTag", "Admin_Order_history_n_upcoming: onCreate started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_order_history_n_upcoming);
 
         TabLayout tabLayout = findViewById(R.id.tablayout);
         ViewPager2 viewPager = findViewById(R.id.viewpager);
 
-        // Create a list of fragments
         List<Fragment> fragments = new ArrayList<>();
         fragments.add(new Admin_UpcomingOrdersFragment());
         fragments.add(new Admin_OrderHistoryFragment());
 
-        // Create a new instance of the VPAdapter class
-        VPAdapter vpAdapter = new VPAdapter(this, fragments);
-
-        viewPager.setAdapter(vpAdapter);
+        VPAdapter adapter = new VPAdapter(this, fragments);
+        viewPager.setAdapter(adapter);
 
         new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
             if (position == 0) {
